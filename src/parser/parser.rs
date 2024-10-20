8238300f02ab57f016ccc446257b16eca507c342
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-10-20 23:20:40 +0100

    Added parser error

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,26 @@
+use std::fmt::Display;
+
 use super::{
     ast::{AstNode, Literal, Object, Property},
     token::{Token, TokenType},
 };
+
+#[derive(Debug)]
+pub enum ParserError {
+    UnexpectedToken(String),
+}
+
+impl std::error::Error for ParserError { }
+
+impl Display for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnexpectedToken(error_message) => {
+                write!(f, "{}", error_message.as_str())
+            }
+        }
+    }
+}
 
 pub struct Parser {
     current: usize,
@@ -13,40 +32,39 @@ impl Parser {
         Self { current: 0, tokens }
     }
 
-    pub fn parse(&mut self) -> impl AstNode {
+    pub fn parse(&mut self) -> Result<impl AstNode, ParserError> {
         self.parse_object()
     }
 
-    fn parse_object(&mut self) -> impl AstNode {
+    fn parse_object(&mut self) -> Result<impl AstNode, ParserError> {
         let left = self.get_current_advance().clone();
 
         let mut properties = vec![];
 
         if self.get_current_token().token_type != TokenType::RightBrace {
-            properties.push(self.parse_property());
+            properties.push(self.parse_property()?);
 
             while self.get_current_token().token_type == TokenType::Comma {
                 self.get_current_advance();
-                properties.push(self.parse_property());
+                properties.push(self.parse_property()?);
             }
         }
 
         let right = self.get_current_advance().clone();
 
-        Object::new(left, properties, right)
+        Ok(Object::new(left, properties, right))
     }
 
-    fn parse_property(&mut self) -> Property {
+    fn parse_property(&mut self) -> Result<Property, ParserError> {
         let key = self
-            .get_current_or_error(TokenType::Identifier, "Expected identifier")
-            .unwrap()
+            .get_current_or_error(TokenType::Identifier, "Expected identifier")?
             .clone()
             .literal;
 
         let colon = self.get_current_advance().clone();
         let value = self.parse_literal();
 
-        Property::new(key, colon, value)
+        Ok(Property::new(key, colon, value))
     }
 
     fn parse_literal(&mut self) -> Literal {
@@ -57,13 +75,13 @@ impl Parser {
         &mut self,
         token_type: TokenType,
         error: &str,
-    ) -> Result<&Token, String> {
+    ) -> Result<&Token, ParserError> {
         if token_type == self.get_current_token().token_type {
             self.current += 1;
             return Ok(self.get_current_token());
         }
 
-        Err(error.to_string())
+        Err(ParserError::UnexpectedToken(error.to_string()))
     }
 
     fn get_current_advance(&mut self) -> &Token {
